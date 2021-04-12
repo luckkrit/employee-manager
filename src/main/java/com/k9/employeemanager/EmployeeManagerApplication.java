b88c298e53e610feb81ab5a98ea0977e404345c8
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-12 22:12:40 +0700

    Init Project with Model

---
+++ src/main/java/com/k9/employeemanager/EmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.k9.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagerApplication.class, args);
+    }
+
+}
