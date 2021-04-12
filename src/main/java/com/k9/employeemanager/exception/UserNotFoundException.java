9294b16177013aac43b20e8259b5237a7d777d52
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-12 23:24:31 +0700

    Implement EmployeeService and custom functions

---
+++ src/main/java/com/k9/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.k9.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/k9/employeemanager/service/EmployeeService.java
@@ -0,0 +1,41 @@
+package com.k9.employeemanager.service;
+
+import com.k9.employeemanager.exception.UserNotFoundException;
+import com.k9.employeemanager.model.Employee;
+import com.k9.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return this.employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepository.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteEmployeeById(id);
+    }
+}
