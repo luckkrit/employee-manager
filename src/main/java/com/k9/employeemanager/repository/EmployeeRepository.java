a0af793a8535ab5106ca55c6d191b4c1af188b26
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-15 10:28:06 +0700

    Add controller, enable CORS and include http client for Testing.

--- src/main/java/com/k9/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/com/k9/employeemanager/repository/EmployeeRepository.java
@@ -3,8 +3,10 @@ package com.k9.employeemanager.repository;
 import com.k9.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
+@Transactional
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     void deleteEmployeeById(Long id);
 
